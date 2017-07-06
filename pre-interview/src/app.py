6bf1cb83b4aeefeba628569a3d7f393440f7504e
Clement _dhwty_ <clemwek@gmail.com>
2017-07-06 15:41:58 +0300

    Add endpoints for all the pages

--- pre-interview/src/app.py
+++ pre-interview/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
@@ -8,6 +8,35 @@ app = Flask(__name__)
 def home():
     return render_template('welcome.html')
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        print(request.form['inputPassword'])
+        print(request.form['inputEmail'])
+        if request.form['inputPassword'] != 'admin' or request.form['inputEmail'] != 'admin@test.com':
+            error = 'Invalid credentials, please try again.'
+        else:
+            return redirect(url_for('bucketlist'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/bucketlist')
+def bucketlist():
+    return render_template('bucketlist.html')
+
+
+@app.route('/add_activity/{bucket_id}')
+def add_activity(bucket_id):
+    return render_template('add_activity.html')
+
+
+@app.route('/add_bucketlist', methods=['GET', 'POST'])
+def add_bucketlist():
+    if request.method == 'POST':
+        return redirect(url_for('bucketlist'))
+    return render_template('add_bucketlist.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- pre-interview/src/tests/test_app.py
+++ pre-interview/src/tests/test_app.py
@@ -8,9 +8,21 @@ class TestBucketListApp(unittest.TestCase):
     #     return src.app.test_client()
 
     def test_urls(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
+        client = app.test_client(self)
+        welcome = client.get('/', content_type='html/text')
+        self.assertEqual(welcome.status_code, 200)
+
+        login = client.get('/login', content_type='html/text')
+        self.assertEqual(login.status_code, 200)
+
+        bucketlist = client.get('/bucketlist', content_type='html/text')
+        self.assertEqual(bucketlist.status_code, 200)
+
+        add_bucketlist = client.get('/add_bucketlist', content_type='html/text')
+        self.assertEqual(add_bucketlist.status_code, 200)
+
+        add_activity = client.get('/add_activity', content_type='html/text')
+        self.assertEqual(add_activity.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
