f253987bdf0f2e87961e3d36587fce8ec8e8a1c1
Clement _dhwty_ <clemwek@gmail.com>
2017-07-07 13:52:20 +0300

    Add loggin and register

--- pre-interview/src/app.py
+++ pre-interview/src/app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template, request, redirect, url_for
-
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
+from models.users.user import User
 
 app = Flask(__name__)
+app.secret_key = 'supersecret'
+
+
+app.users = {}
+app.bucketlist = {}
+app.activity = {}
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
 
 
 @app.route('/')
@@ -13,26 +31,59 @@ def home():
 def login():
     error = None
     if request.method == 'POST':
-        print(request.form['inputPassword'])
-        print(request.form['inputEmail'])
-        if request.form['inputPassword'] != 'admin' or request.form['inputEmail'] != 'admin@test.com':
+        email = request.form['inputEmail']
+        password = request.form['inputPassword']
+        if email not in app.users or password != app.users[email].password:
             error = 'Invalid credentials, please try again.'
         else:
+            session['logged_in'] = True
+            session['email'] = email
+            flash('You are logged in')
             return redirect(url_for('bucketlist'))
     return render_template('login.html', error=error)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+        name = request.form['inputName'].lower()
+        email = request.form['inputEmail'].lower()
+        password = request.form['inputPassword'].lower()
+        passwordAgain = request.form['inputPasswordAgain'].lower()
+
+        if password == passwordAgain:
+            app.users[email] = User(name, email, password)
+            session['logged_in'] = True
+            session['email'] = email
+            flash('You are logged in')
+            return redirect(url_for('bucketlist'))
+        error = 'password do not march'
+
+    return render_template('register.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('you are logged out.')
+    return redirect(url_for('home'))
+
+
 @app.route('/bucketlist')
+@login_required
 def bucketlist():
     return render_template('bucketlist.html')
 
 
 @app.route('/add_activity/{bucket_id}')
+@login_required
 def add_activity(bucket_id):
     return render_template('add_activity.html')
 
 
 @app.route('/add_bucketlist', methods=['GET', 'POST'])
+@login_required
 def add_bucketlist():
     if request.method == 'POST':
         return redirect(url_for('bucketlist'))
--- pre-interview/src/tests/test_app.py
+++ pre-interview/src/tests/test_app.py
@@ -1,11 +1,28 @@
 import unittest
-import flask
 from src.app import app
 
 
 class TestBucketListApp(unittest.TestCase):
-    # def client(self):
-    #     return src.app.test_client()
+    def register(self, email, password, name):
+        client = app.test_client(self)
+        return client.post(
+            '/register',
+            data=dict(inputEmail=email, inputPassword=password, inputName=name),
+            follow_redirects=True
+        )
+
+    def login(self, email, password):
+        return self.app.post(
+            '/login',
+            data=dict(email=email, password=password),
+            follow_redirects=True
+        )
+
+    def logout(self):
+        return self.app.get(
+            '/logout',
+            follow_redirects=True
+        )
 
     def test_urls(self):
         client = app.test_client(self)
@@ -16,13 +33,22 @@ class TestBucketListApp(unittest.TestCase):
         self.assertEqual(login.status_code, 200)
 
         bucketlist = client.get('/bucketlist', content_type='html/text')
-        self.assertEqual(bucketlist.status_code, 200)
+        # app.login('admin@test.com', 'admin')
+        self.assertEqual(bucketlist.status_code, 302)
 
         add_bucketlist = client.get('/add_bucketlist', content_type='html/text')
-        self.assertEqual(add_bucketlist.status_code, 200)
+        self.assertEqual(add_bucketlist.status_code, 302)
 
         # add_activity = client.get('/add_activity', content_type='html/text')
         # self.assertEqual(add_activity.status_code, 200)
+
+    def test_register(self):
+        # client = app.test_client(self)
+        # data = {'inputName': 'john', 'inputEmail': 'test@test.com', 'inputPassword': 'test'}
+        # response = client.post('register', data=data, follow_redirects=True)
+        response = self.register('patkennedy79@gmail.com', 'FlaskIsAwesome', 'FlaskIsAwesome')
+        # self.assertEqual(response.status_code, 200)
+        self.assertIn(b'logged in', response.data)
 
 if __name__ == '__main__':
     unittest.main()
